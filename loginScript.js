a997078f8bf48427a0965dfbedaf17b5646f9c67
Yefrit1 <127126706+Yefrit1@users.noreply.github.com>
2023-11-17 15:54:59 -0500

    Add files via upload

---
+++ loginScript.js
@@ -0,0 +1,21 @@
+document.getElementById('button-menu').addEventListener('click', function() {
+  var menu = document.getElementById('side-menu');
+  menu.style.width = menu.style.width === '350px' ? '0' : '350px';
+});
+
+function closeNav() {
+  document.getElementById('side-menu').style.width = '0';
+}
+
+function updateDateTime() {
+  var now = new Date();
+  var dateString = now.toDateString();
+  var timeString = now.toLocaleTimeString();
+
+  document.getElementById('current-date').textContent = dateString;
+  document.getElementById('current-time').textContent = timeString;
+}
+
+setInterval(updateDateTime, 1000);
+
+updateDateTime();
